7721300461cb120de738d72ec22275e7ba0f0a2a
headshot2017 <>
2025-07-07 17:11:58 -0400

    unnecessary using directives

--- StageLoaderComponent.cs
+++ StageLoaderComponent.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 using UnityEngine.Networking;
 using MelonLoader;
-using UnityEngine.SceneManagement;
-using System.Xml.Linq;
-using UnityEngine.UIElements;
 
 namespace StageLoader
 {
