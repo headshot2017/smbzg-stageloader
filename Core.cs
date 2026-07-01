1088de058e19e0766876e61245106aae48e850b1
headshot2017 <>
2026-06-30 22:33:47 -0400

    remove stage dropdown UI, add stages to grid
    * removed stage dropdown UI that required mouse and didn't support voting with 2+ players
    * turn the stage grid into a scrollable region
    * add all custom stages to the grid
    
    this allows voting for custom stages with 2+ players

--- Core.cs
+++ Core.cs
@@ -1,8 +1,8 @@
-﻿using MelonLoader;
-using HarmonyLib;
-using UnityEngine;
-using System.Reflection;
+﻿using HarmonyLib;
+using MelonLoader;
 using SMBZG.CharacterSelect;
+using System.Reflection;
+using UnityEngine;
 using UnityEngine.UI;
 
 [assembly: MelonInfo(typeof(StageLoader.Core), "StageLoader", "1.6.0", "Headshotnoby/headshot2017", null)]
@@ -63,218 +63,71 @@ namespace StageLoader
 
         public void LoadCustomStageUI()
         {
-            Font arial = null;
-            Font[] fonts = Resources.FindObjectsOfTypeAll<Font>();
-            foreach (Font font in fonts)
-            {
-                if (font.name == "Arial")
-                {
-                    arial = font;
-                    break;
-                }
-            }
-
-            Sprite uisprite = null;
-            Sprite uiarrow = null;
-            Sprite[] sprites = Resources.FindObjectsOfTypeAll<Sprite>();
-            foreach (Sprite sprite in sprites)
-            {
-                if (sprite.name == "UISprite")
-                    uisprite = sprite;
-                if (sprite.name == "DropdownArrow")
-                    uiarrow = sprite;
-            }
-
-            // Setup the UI
-
-            // Find a dropdown prefab
-            // Go through each children until it's found
-            GameObject dropdownTemplate = null;
-            Stack<Transform> objs = new Stack<Transform>();
-            objs.Push(CharacterSelectScript.ins.Section_BattleSettings.transform);
-            while (objs.Count > 0)
-            {
-                Transform obj = objs.Pop();
-
-                if (obj.GetComponent<Dropdown>() != null && obj.Find("Template") != null)
-                {
-                    dropdownTemplate = obj.Find("Template").gameObject;
-                    objs.Clear();
-                    break;
-                }
-
-                for (int i = 0; i < obj.transform.childCount; i++)
-                    objs.Push(obj.GetChild(i));
-            }
-
-            if (!dropdownTemplate)
-            {
-                LoggerInstance.Msg($"================ WARNING ================");
-                LoggerInstance.Msg($"UI DROPDOWN TEMPLATE NOT FOUND");
-                LoggerInstance.Msg($"Custom stage selector will NOT be visible");
-                LoggerInstance.Msg($"=========================================");
-                return;
-            }
-
-            // Part 1: GameObjects
             GameObject root = CharacterSelectScript.ins.Section_StageSelect;
-            GameObject CustomStageRoot = new GameObject("Custom Stage UI");
 
-            GameObject CustomStageLabelObj = new GameObject("Custom Stage Label");
-            GameObject CustomStageLabelImgObj = new GameObject("Custom Stage Label Img");
-            GameObject CustomStageLabelTextObj = new GameObject("Custom Stage Label Text");
+            Transform Text_SelectStage = root.transform.Find("Text_SelectStage");
+            GameObject StageGrid = root.transform.Find("StageGrid").gameObject;
 
-            GameObject CustomStageListObj = new GameObject("Custom Stage List");
-            GameObject CustomStageListTextObj = new GameObject("Custom Stage List Text");
-            GameObject CustomStageListArrowObj = new GameObject("Custom Stage List Arrow");
-            GameObject CustomStageListTemplate = GameObject.Instantiate(dropdownTemplate);
+            // move Canvas/StageSelectPage/Text_SelectStage transform.y += 50
+            Text_SelectStage.transform.position += new Vector3(0, 50);
 
-            GameObject CustomStageConfirmObj = new GameObject("Custom Stage Confirm");
-            GameObject CustomStageConfirmTextObj = new GameObject("Custom Stage Confirm Text");
+            // instantiate Scroll View gameobject from CharacterSelectPage
+            // if any, remove all children from Scroll View(Clone)/ Viewport / Content
+            // (need to use DestroyImmediate instead of Destroy, otherwise an exception occurs)
+            GameObject ScrollView = GameObject.Instantiate(CharacterSelectScript.ins.Section_CharacterSelect.transform.Find("Scroll View").gameObject);
+            GameObject Viewport = ScrollView.transform.Find("Viewport").gameObject;
+            GameObject Content = Viewport.transform.Find("Content").gameObject;
+            while (Content.transform.childCount > 0)
+                GameObject.DestroyImmediate(Content.transform.GetChild(0).gameObject);
 
-            GameObject CustomStageReloadObj = new GameObject("Custom Stage Reload");
-            GameObject CustomStageReloadTextObj = new GameObject("Custom Stage Reload Text");
+            // set parent of Scroll View(Clone) to StageSelectPage
+            ScrollView.transform.SetParent(root.transform);
 
+            // set parents of StageGrid's children to Scroll View(Clone)/Viewport/Content
+            while (StageGrid.transform.childCount > 0)
+                StageGrid.transform.GetChild(0).SetParent(Content.transform);
 
-            // Part 2: Children setup
-            CustomStageRoot.transform.parent = root.transform;
-            CustomStageRoot.transform.localPosition = new Vector3(-50, -400);
-            CustomStageRoot.transform.localScale = Vector3.one;
+            GameObject.Destroy(StageGrid.gameObject);
 
-            CustomStageLabelObj.transform.parent = CustomStageRoot.transform;
-            CustomStageLabelObj.transform.localPosition = new Vector3(-350, 0);
-            CustomStageLabelObj.transform.localScale = Vector3.one;
-            CustomStageLabelImgObj.transform.parent = CustomStageLabelObj.transform;
-            CustomStageLabelImgObj.transform.localPosition = Vector3.zero;
-            CustomStageLabelImgObj.transform.localScale = Vector3.one;
-            CustomStageLabelTextObj.transform.parent = CustomStageLabelObj.transform;
-            CustomStageLabelTextObj.transform.localPosition = Vector3.zero;
-            CustomStageLabelTextObj.transform.localScale = Vector3.one;
+            // set local position to 0,0,0 of: Scroll View(Clone), Viewport, Content
+            ScrollView.transform.localPosition = Vector3.zero;
+            Viewport.transform.localPosition = Vector3.zero;
+            Content.transform.localPosition = Vector3.zero;
 
-            CustomStageListObj.transform.parent = CustomStageRoot.transform;
-            CustomStageListObj.transform.localPosition = Vector3.zero;
-            CustomStageListObj.transform.localScale = Vector3.one;
-            CustomStageListTextObj.transform.parent = CustomStageListObj.transform;
-            CustomStageListTextObj.transform.localPosition = new Vector3(8, 0);
-            CustomStageListTextObj.transform.localScale = Vector3.one;
-            CustomStageListArrowObj.transform.parent = CustomStageListObj.transform;
-            CustomStageListArrowObj.transform.localPosition = new Vector3(235, 0);
-            CustomStageListArrowObj.transform.localScale = Vector3.one;
-            CustomStageListTemplate.transform.SetParent(CustomStageListObj.transform, false);
-            CustomStageListTemplate.transform.localPosition = Vector3.zero;
-            CustomStageListTemplate.transform.localScale = Vector3.one;
+            // set Scroll View(Clone) RectTransform.sizeDelta to 0,-300
+            ScrollView.GetComponent<RectTransform>().sizeDelta = new Vector2(0, -300);
 
-            CustomStageConfirmObj.transform.parent = CustomStageRoot.transform;
-            CustomStageConfirmObj.transform.localPosition = new Vector3(310, 0);
-            CustomStageConfirmObj.transform.localScale = Vector3.one;
-            CustomStageConfirmTextObj.transform.parent = CustomStageConfirmObj.transform;
-            CustomStageConfirmTextObj.transform.localPosition = Vector3.zero;
-            CustomStageConfirmTextObj.transform.localScale = Vector3.one;
-
-            CustomStageReloadObj.transform.parent = CustomStageRoot.transform;
-            CustomStageReloadObj.transform.localPosition = new Vector3(440, 0);
-            CustomStageReloadObj.transform.localScale = Vector3.one;
-            CustomStageReloadTextObj.transform.parent = CustomStageReloadObj.transform;
-            CustomStageReloadTextObj.transform.localPosition = Vector3.zero;
-            CustomStageReloadTextObj.transform.localScale = Vector3.one;
+            // set Scroll View(Clone)/ Viewport / Content GridLayoutGroup.cellSize to 230,170(or 208.3333 149.4) and GridLayoutGroup.constraintCount to 6(FixedColumnCount)
+            GridLayoutGroup grid = Content.GetComponent<GridLayoutGroup>();
+            grid.cellSize = new Vector2(208.3333f, 149.4f);
+            grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            grid.constraintCount = 6;
+            grid.padding.top = 3;
+            grid.padding.bottom = 3;
 
 
-            // Part 3: Components
-            RectTransform CustomStageLabelTextTr = CustomStageLabelTextObj.AddComponent<RectTransform>();
-            RectTransform CustomStageLabelImgTr = CustomStageLabelImgObj.AddComponent<RectTransform>();
-            Text CustomStageLabelText = CustomStageLabelTextObj.AddComponent<Text>();
-            Image CustomStageLabelImg = CustomStageLabelImgObj.AddComponent<Image>();
-            CustomStageLabelTextTr.sizeDelta = new Vector2(150, 32);
-            CustomStageLabelImgTr.sizeDelta = new Vector2(150, 32);
-            CustomStageLabelText.text = "Custom Stages";
-            CustomStageLabelText.color = Color.black;
-            CustomStageLabelText.fontSize = 18;
-            CustomStageLabelText.fontStyle = FontStyle.Bold;
-            CustomStageLabelText.alignment = TextAnchor.MiddleCenter;
-            CustomStageLabelText.font = arial;
-            CustomStageLabelImg.color = new Color(0.75f, 0.75f, 0.75f);
-            CustomStageLabelImg.sprite = uisprite;
-            CustomStageLabelImg.type = Image.Type.Sliced;
+            // add all the stages!
+            for (int i=0; i<customStages.Count; i++)
+            {
+                StageData stage = customStages[i];
+                GameObject stageClone = GameObject.Instantiate(Content.transform.GetChild(1).gameObject, Content.transform);
+                StageSelector stageSelectorComp = stageClone.GetComponent<StageSelector>();
+                Image stageImage = stageClone.transform.Find("Mask").Find("Image").GetComponent<Image>();
+                Text stageText = stageClone.transform.Find("Image").GetComponentInChildren<Text>();
 
-            RectTransform CustomStageListTr = CustomStageListObj.AddComponent<RectTransform>();
-            RectTransform CustomStageListTextTr = CustomStageListTextObj.AddComponent<RectTransform>();
-            RectTransform CustomStageListArrowTr = CustomStageListArrowObj.AddComponent<RectTransform>();
-            Image CustomStageListImg = CustomStageListObj.AddComponent<Image>();
-            Dropdown CustomStageListDropdown = CustomStageListObj.AddComponent<Dropdown>();
-            Text CustomStageListText = CustomStageListTextObj.AddComponent<Text>();
-            Image CustomStageListArrowImg = CustomStageListArrowObj.AddComponent<Image>();
-            CustomStageListTr.sizeDelta = new Vector2(500, 32);
-            CustomStageListTextTr.sizeDelta = new Vector2(500, 32);
-            CustomStageListArrowTr.sizeDelta = new Vector2(20, 20);
-            CustomStageListImg.sprite = uisprite;
-            CustomStageListImg.type = Image.Type.Sliced;
-            CustomStageListDropdown.transition = Selectable.Transition.ColorTint;
-            CustomStageListDropdown.image = CustomStageListImg;
-            CustomStageListDropdown.captionText = CustomStageListText;
-            CustomStageListDropdown.itemText = CustomStageListTemplate.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).gameObject.GetComponent<Text>();
-            CustomStageListDropdown.template = CustomStageListTemplate.GetComponent<RectTransform>();
-            CustomStageListText.text = "item";
-            CustomStageListText.color = Color.black;
-            CustomStageListText.fontSize = 18;
-            CustomStageListText.alignment = TextAnchor.MiddleLeft;
-            CustomStageListText.font = arial;
-            CustomStageListArrowImg.sprite = uiarrow;
+                stageClone.name = stage.name;
+                stageSelectorComp.ChooseRandomStage = false;
+                stageSelectorComp.FixedBattleBackgroundSelection = true;
+                stageSelectorComp.BattleBackgroundIndex = 0;
+                stageSelectorComp.Stage = (BattleCache.StageEnum)(i + 100);
+                stageImage.enabled = false;
+                stageText.text = stage.name;
 
-            RectTransform CustomStageConfirmTr = CustomStageConfirmObj.AddComponent<RectTransform>();
-            RectTransform CustomStageConfirmTextTr = CustomStageConfirmTextObj.AddComponent<RectTransform>();
-            Image CustomStageConfirmImg = CustomStageConfirmObj.AddComponent<Image>();
-            Button CustomStageConfirmBtn = CustomStageConfirmObj.AddComponent<Button>();
-            Text CustomStageConfirmText = CustomStageConfirmTextObj.AddComponent<Text>();
-            CustomStageConfirmTr.sizeDelta = new Vector2(64, 32);
-            CustomStageConfirmTextTr.sizeDelta = new Vector2(64, 32);
-            CustomStageConfirmImg.sprite = uisprite;
-            CustomStageConfirmImg.type = Image.Type.Sliced;
-            CustomStageConfirmBtn.image = CustomStageConfirmImg;
-            CustomStageConfirmText.text = "Go";
-            CustomStageConfirmText.color = Color.black;
-            CustomStageConfirmText.fontSize = 18;
-            CustomStageConfirmText.fontStyle = FontStyle.Bold;
-            CustomStageConfirmText.alignment = TextAnchor.MiddleCenter;
-            CustomStageConfirmText.font = arial;
+                CharacterSelectScript.ins.StageList.Add(stageSelectorComp);
+            }
 
-            RectTransform CustomStageReloadTr = CustomStageReloadObj.AddComponent<RectTransform>();
-            RectTransform CustomStageReloadTextTr = CustomStageReloadTextObj.AddComponent<RectTransform>();
-            Image CustomStageReloadImg = CustomStageReloadObj.AddComponent<Image>();
-            Button CustomStageReloadBtn = CustomStageReloadObj.AddComponent<Button>();
-            Text CustomStageReloadText = CustomStageReloadTextObj.AddComponent<Text>();
-            CustomStageReloadTr.sizeDelta = new Vector2(192, 32);
-            CustomStageReloadTextTr.sizeDelta = new Vector2(192, 32);
-            CustomStageReloadImg.sprite = uisprite;
-            CustomStageReloadImg.type = Image.Type.Sliced;
-            CustomStageReloadBtn.image = CustomStageReloadImg;
-            CustomStageReloadText.text = "Refresh stage list";
-            CustomStageReloadText.color = Color.black;
-            CustomStageReloadText.fontSize = 18;
-            CustomStageReloadText.fontStyle = FontStyle.Bold;
-            CustomStageReloadText.alignment = TextAnchor.MiddleCenter;
-            CustomStageReloadText.font = arial;
-
-
-            List<string> stageNames = new List<string>();
-            foreach (StageData stage in customStages)
-                stageNames.Add(stage.name);
-
-            CustomStageListDropdown.AddOptions(stageNames);
-            CustomStageConfirmBtn.onClick.AddListener(OnCustomStageGo);
-            CustomStageReloadBtn.onClick.AddListener(OnCustomStageRefresh);
-            stageDropdown = CustomStageListDropdown;
-        }
-
-        void OnCustomStageGo()
-        {
-            BattleCache.StageEnum? stage = 0;
-            typeof(CharacterSelectScript).GetField("SelectedStage", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(CharacterSelectScript.ins, stage);
-            CharacterSelectScript.ins.OnSubmit();
-        }
-
-        void OnCustomStageRefresh()
-        {
-            LoadCustomStages();
+            // go back to top
+            ScrollView.GetComponent<ScrollRect>().verticalNormalizedPosition = 1;
         }
 
         [HarmonyPatch(typeof(BattleCache), "Stage_GetData", new Type[] { typeof(BattleCache.StageEnum) })]
@@ -337,30 +190,11 @@ namespace StageLoader
         {
             private static bool Prefix(BattleBackgroundManager __instance, StageData stage, BattleMusicData song, int? battleBackgroundIndex = null)
             {
-                bool isArcade = (bool)typeof(GC).GetProperty("IsInArcadeMode", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(GC.ins);
                 SMBZG.BattleSettings BattleSettings = (SMBZG.BattleSettings)typeof(GC).GetField("BattleSetting", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(GC.ins);
                 BattleCache.StageEnum? SelectedStage = BattleSettings.Stage;
 
-                StageLoaderEditor.isCustomStage = SelectedStage == 0;
-                if (isArcade || !StageLoaderEditor.isCustomStage)
-                    return true;
-
-                FieldInfo ActiveStage = typeof(BattleBackgroundManager).GetField("ActiveStage", BindingFlags.Instance | BindingFlags.NonPublic);
-                FieldInfo ActiveSong = typeof(BattleBackgroundManager).GetField("ActiveSong", BindingFlags.Instance | BindingFlags.NonPublic);
-                MethodInfo SetBattleBackground = typeof(BattleBackgroundManager).GetMethod("SetBattleBackground", BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(BattleBackgroundData) }, null);
-
-                Melon<Core>.Logger.Msg($"Playing custom stage: {customStages[stageDropdown.value].name}");
-
-                StageData customStage = customStages[stageDropdown.value];
-                ActiveStage.SetValue(__instance, customStage);
-                ActiveSong.SetValue(__instance, song);
-                //BattleBackgroundData battleBackground = ((!battleBackgroundIndex.HasValue) ? stage.BattleBackgroundDataList.GetRandom() : stage.BattleBackgroundDataList[battleBackgroundIndex.Value]);
-                //BattleBackgroundData battleBackground = customStage.BattleBackgroundDataList.GetRandom();
-                BattleBackgroundData battleBackground = customStage.BattleBackgroundDataList[0];
-                SetBattleBackground.Invoke(__instance, new object[] { battleBackground });
-
-                Melon<Core>.Logger.Msg($"{ActiveStage.GetValue(__instance)}");
-                return false;
+                StageLoaderEditor.isCustomStage = !SelectedStage.HasValue || (int)SelectedStage >= 100;
+                return true;
             }
         }
     }
